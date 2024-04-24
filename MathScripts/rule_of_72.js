e59a758bbb01aa837126cee95bec3fd013868faf
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-04-24 14:23:47 -0500

    finished rule_of_72.js

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,9 @@
+// formula
+let savings = 5000;
+let interestRate = 10;
+
+let doubledBalance = (savings * 2).toFixed(2);
+let timeToDouble = 72/ interestRate;
+
+// console output
+console.log("At a " + interestRate + "% interest rate, your savings account will be worth $" + doubledBalance + " in " + timeToDouble + " years.");
