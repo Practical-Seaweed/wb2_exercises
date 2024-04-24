74d872387c7f30c3d61628efd374b75d7d51b3c6
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-04-24 11:50:16 -0500

    finished tip_amount.js

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,7 @@
+// formula
+let foodBill = 150;
+let tipPercent = 10;
+let tipAmount = (foodBill * tipPercent)/ 100;
+
+//console output
+console.log("The tip on a $" + foodBill + " food bill is $" + tipAmount )
