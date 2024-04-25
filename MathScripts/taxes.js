5bf8e332cf6ea48141817c7fafb8ab39f64432ef
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-04-25 09:39:44 -0500

    finished taxes.js

---
+++ MathScripts/taxes.js
@@ -0,0 +1,8 @@
+// formula
+
+let moneyAmount = 3000; // $3,000
+let federalTax = .23;  //23%
+let taxWithheld = federalTax * moneyAmount;
+
+// console output
+console.log("The amount withheld for federal taxes is $" + taxWithheld.toFixed(2))
